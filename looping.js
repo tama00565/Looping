ea6e38a3ee15ca9ce86dc767550353c4467f1829
tama00565 <kakatama0606@gmail.com>
2024-01-31 14:05:59 +0700

    Loopingg

---
+++ looping.js
@@ -0,0 +1,59 @@
+// * pengulangan 
+for(let number = 1; number <= 20; number++) {
+    console.log(number);
+}
+
+// loping for
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// looping while
+// let a = 1;
+// while(a < 1){
+//     console.log(a);
+//     a++;
+// }
+
+// looping do while
+
+
+// -------------------------------------------------------
+// for(let g = 1; g <= 20; g++) {
+//     if( g % 2 == 0){
+//         console.log(`${g} genap`);
+//     }else {
+//         console.log(`${g} ganjil`);
+//     }
+// }
+
+// -------------------------------------------------------
+// let j = 1;
+// while(j < 51){
+//     if( j % 2 == 0){
+//         console.log (`${j} Genap`)
+//     }else {
+//         console.log (`${j} Ganjil`)
+//     }
+//     j++;
+// } 
+
+// -------------------------------------------------------
+// let n = 5;
+// let hasil = 1;
+
+// for (let i = 2; i <= n; i++) {
+//   hasil *= i;
+// }
+// console.log(`Faktorial dari 5 adalah: ${hasil}`);
+
+// -------------------------------------------------------
+// let g = 5;
+// let gus = 1;
+// let i = 2;
+
+// while (i <= g) {
+//   gus *= i;
+//   i++;
+// }
+// console.log(`Faktorial dari 5 adalah: ${gus}`);
